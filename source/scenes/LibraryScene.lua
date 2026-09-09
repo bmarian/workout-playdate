aadbebbc7058295439763918c36d88d5370a4a24
bmarian <33843056+bmarian@users.noreply.github.com>
2026-09-09 13:33:40 +0300

    WIP

--- source/scenes/LibraryScene.lua
+++ source/scenes/LibraryScene.lua
@@ -10,7 +10,7 @@ function scene:drawBackground()
 	scene.super.drawBackground(self)
 
 	Noble.Text.draw("Workout Playdate", CENTER_X, 96, Noble.Text.ALIGN_CENTER, false, Noble.Text.FONT_LARGE)
-	Noble.Text.draw("Press A to start", CENTER_X, 136, Noble.Text.ALIGN_CENTER)
+	Noble.Text.draw("Press B to start", CENTER_X, 136, Noble.Text.ALIGN_CENTER)
 end
 
 scene.inputHandler = {
